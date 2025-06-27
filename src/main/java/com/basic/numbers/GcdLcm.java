6017f6f0e290ef2a4e85800c2b9365a57071b3da
Hitesh Suryawanshi <109617242+Hitesh-coep@users.noreply.github.com>
2025-06-27 23:14:07 +0530

    chore: add 5 more problems from category 1

---
+++ src/main/java/com/basic/numbers/GcdLcm.java
@@ -0,0 +1,20 @@
+package com.basic.numbers;
+
+public class AllDivisors {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 18;
+        System.out.println(gcd(a,b));
+        System.out.println(lcm(a,b));
+    }
+    public static int gcd(int a, int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b, a%b);
+    }
+    public static int lcm(int a, int b){
+        int lcm = a*b/gcd(a,b);
+        return lcm;
+    }
+}
